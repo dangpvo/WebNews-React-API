6fecccdb00f7839b4d32edade0fb41b99967dfa0
Dennis Vo <dennisphuc@gmail.com>
2025-04-16 11:03:39 +0200

    feat: finish loading the news json data from newsapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import NewsBoard from "./components/NewsBoard";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <NewsBoard />
+    </div>
+  );
 };
 
 export default App;
