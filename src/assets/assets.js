449186677464e4cb1b5842bd9343ce149c6dfdbd
Dennis Vo <dennisphuc@gmail.com>
2025-04-16 11:15:53 +0200

    feat: load news data to a simple grid

---
+++ src/assets/assets.js
@@ -0,0 +1,3 @@
+import img_placeholder from "./img_placeholder.jpg";
+
+export const assets = { img_placeholder };
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const NewsItem = ({ title, description, src, url }) => {
   return (
     <div
-      className="card bg-dark text-light mb-3 d-inline-block"
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
       style={{ maxWidth: "345px" }}
     >
-      <img src={src} className="card-img-top" alt="..." />
+      <img
+        src={src ? src : assets.img_placeholder}
+        style={{ height: "200px", width: "100%" }}
+        className="card-img-top"
+        alt="..."
+      />
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">
+          {description ? description.slice(0, 90) : "custom description"}
+        </p>
         <a href={url} className="btn btn-primary">
           Read more
         </a>
